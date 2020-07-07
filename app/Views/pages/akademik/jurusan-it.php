c1ede8241a6cad0aa2fdf9ad3d1d3aeb5f4ce5ec
Yosyafat Aulia Ferdiansyah <alferdiansyah@it.student.pens.ac.id>
2020-07-08 06:58:12 +0700

    bikin konten sm link di hlmn akademik

--- app/Views/pages/akademik/jurusan-it.php
+++ app/Views/pages/akademik/jurusan-it.php
@@ -86,11 +86,47 @@
                 </aside>
             </div>
             <div class="col-md-8">
-                <div class="berita-content">
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus vitae quibusdam iste, qui, beatae fugiat sit repudiandae voluptates quas architecto aliquid nam dolor laudantium at, molestiae cum ipsa maiores quidem excepturi sed? Ut commodi nulla suscipit consequatur, eveniet est, necessitatibus praesentium labore aperiam tenetur earum ratione illum repudiandae inventore. Veritatis.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum animi voluptates est autem alias aspernatur repellat accusamus repudiandae laborum molestiae ex eligendi, nam rerum voluptatem ea culpa magnam. Nulla officiis temporibus repellat. Laboriosam, consequatur perspiciatis quod mollitia vero earum iusto sapiente architecto ex laborum sit rem ullam? Dolor, ad neque?</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum animi voluptates est autem alias aspernatur repellat accusamus repudiandae laborum molestiae ex eligendi, nam rerum voluptatem ea culpa magnam. Nulla officiis temporibus repellat. Laboriosam, consequatur perspiciatis quod mollitia vero earum iusto sapiente architecto ex laborum sit rem ullam? Dolor, ad neque?</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum animi voluptates est autem alias aspernatur repellat accusamus repudiandae laborum molestiae ex eligendi, nam rerum voluptatem ea culpa magnam. Nulla officiis temporibus repellat. Laboriosam, consequatur perspiciatis quod mollitia vero earum iusto sapiente architecto ex laborum sit rem ullam? Dolor, ad neque?</p>
+                <div class="akademik-content">
+                    <h5 class="heading mb-3">Informasi Umum</h5>
+                    <p>Diawali tahun 2000, PENS menerima mahasiswa jenjang D3 Program Studi Teknologi Informasi melalui Keputusan Direktur Jenderal Pendidikan Tinggi No. 244/DIKTI/Kep/2000 pada tanggal 27 Juli 2000. Dan pada tahun 2003 PENS menerima mahasiswa untuk jenjang D4 (4 tahun) Program Studi Teknologi Informasi melalui Keputusan Direktur Jenderal Pendidikan Tinggi No. 2840/D/T/2005. Dan sejalan dengan Keputusan Direktur Jenderal Pendidikan Tinggi No. 163/DIKTI/Kep/2007 tentang Penataan dan Kodifikasi Program Studi Pada Perguruan Tinggi, sejak tahun 2009 Program Studi Teknologi Informasi berubah nama menjadi Program Studi Teknik Infomatika. Program Studi D3 Teknik Informatika setiap tahun menerima 60 mahasiswa (2 kelas).</p>
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col">
+                                    <span>
+                                        <h5 class="heading">Departemen</h5> : <a href="<?= base_url(); ?>/pages/akademik/dept-tik" class="card-link">Dept. Teknik Informatika dan Komputer</a>
+                                    </span>
+
+                                    <h5 class="heading">Program Akademik</h5>
+                                    <ul class="list-unstyled ml-2">
+                                        <li><a href="<?= base_url(); ?>/pages/akademik/prodi-d3" class="card-link">Diploma III</a></li>
+                                        <li><a href="<?= base_url(); ?>/pages/akademik/prodi-d4" class="card-link">Sarjana Terapan</a></li>
+                                        <li><a href="https://pascasarjana.pens.ac.id" class="card-link">Pascasarjana</a></li>
+                                    </ul>
+
+                                    <h5 class="heading">Program Lainnya</h5>
+                                    <ul class="list-unstyled ml-2">
+                                        <li><a href="https://pmb.pens.ac.id/index.php/lanjut-jenjang-d4" class="card-link">D4 Lanjut Jenjang Teknik Informatika</a></li>
+                                        <li><a href="https://pmb.pens.ac.id/index.php/psdku-pjj-pens" class="card-link">D3 PSDKU PJJ Teknik Informatika</a></li>
+                                        <li><a href="https://pmb.pens.ac.id/index.php/pjj" class="card-link">D3 PJJ Teknik Informatika</a></li>
+                                        <li><a href="https://pmb.pens.ac.id/index.php/pjj/pmb-pjj-2020-seameo/" class="card-link">D3 PJJ Teknik Informatika - SEAMEO</a></li>
+                                        <li><a href="https://pmb.pens.ac.id/index.php/pjj/pmb-lj-pjj-2020-dari-ak/" class="card-link">D3 LJ PJJ Teknik Informatika dari D2 Akademi Komunitas</a></li>
+                                    </ul>
+                                </div>
+                                <div class="col">
+                                    <h5 class="heading">Situs Web</h5>
+                                    <ul class="list-unstyled ml-2">
+                                        <li><a href="https://it.pens.ac.id" class="card-link">https://it.pens.ac.id</a></li>
+                                    </ul>
+                                    <h5 class="heading">Himpunan Mahasiswa</h5>
+                                    <ul class="list-unstyled ml-2">
+                                        <li><a href="https://himit.pens.ac.id" class="card-link">HIMIT</a></li>
+
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- app/Views/pages/akademik/jurusan-tekkom.php
+++ app/Views/pages/akademik/jurusan-tekkom.php
@@ -86,11 +86,23 @@
                 </aside>
             </div>
             <div class="col-md-8">
-                <div class="berita-content">
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus vitae quibusdam iste, qui, beatae fugiat sit repudiandae voluptates quas architecto aliquid nam dolor laudantium at, molestiae cum ipsa maiores quidem excepturi sed? Ut commodi nulla suscipit consequatur, eveniet est, necessitatibus praesentium labore aperiam tenetur earum ratione illum repudiandae inventore. Veritatis.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum animi voluptates est autem alias aspernatur repellat accusamus repudiandae laborum molestiae ex eligendi, nam rerum voluptatem ea culpa magnam. Nulla officiis temporibus repellat. Laboriosam, consequatur perspiciatis quod mollitia vero earum iusto sapiente architecto ex laborum sit rem ullam? Dolor, ad neque?</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum animi voluptates est autem alias aspernatur repellat accusamus repudiandae laborum molestiae ex eligendi, nam rerum voluptatem ea culpa magnam. Nulla officiis temporibus repellat. Laboriosam, consequatur perspiciatis quod mollitia vero earum iusto sapiente architecto ex laborum sit rem ullam? Dolor, ad neque?</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum animi voluptates est autem alias aspernatur repellat accusamus repudiandae laborum molestiae ex eligendi, nam rerum voluptatem ea culpa magnam. Nulla officiis temporibus repellat. Laboriosam, consequatur perspiciatis quod mollitia vero earum iusto sapiente architecto ex laborum sit rem ullam? Dolor, ad neque?</p>
+                <div class="akademik-content">
+                    <h5 class="heading mb-3">Informasi Umum</h5>
+                    <p>Pada saat ini kebutuhan akan teknologi informasi memegang peranan yang sangat penting dan dimasa yang akan datang akan menjadi tulang punggung pertumbuhan ekonomi bangsa. Karena itu, Politeknik Elektronika Negeri Surabaya mendirikan Departemen Teknik Informatika dan Komputer yang terdiri dari empat program studi, yaitu D3 Teknik Informatika, D4 Teknik Informatika, D4 Teknik Komputer, serta S2 Terapan Teknik Informatika dan Komputer. Dengan tujuan menjadi pusat unggulan pendidikan teknologi rekayasa dibidang emerging technology yang terkait bidang Teknologi Informasi dan Komputer untuk menghasilkan lulusan yang siap bersaing dipasar global, Departemen Teknik Informatika dan Komputer telah banyak menghasilkan lulusan yang berkualifikasi unggul dan bekerja sebagai Programmer, Software Engineer, Hardware Designer, Network Administrator, Computer System Analyst, Manager/Supervisor, Services/Technical Support, Marketing/Sales, dan lain-lain.</p>
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col">
+                                    <h5 class="heading">Jurusan</h5>
+                                    <ul class="list-unstyled ml-2">
+                                        <li><a href="<?= base_url(); ?>/pages/akademik/jurusan-it" class="card-link">Teknik Informatika</a></li>
+                                        <li><a href="<?= base_url(); ?>/pages/akademik/jurusan-tekkom" class="card-link">Teknik Komputer</a></li>
+                                    </ul>
+                                </div>
+                                <div class="col"></div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
