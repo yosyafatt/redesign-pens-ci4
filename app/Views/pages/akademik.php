886cd2603142e85f7abab9ba0c7c6f6df069390c
Yosyafat Aulia Ferdiansyah <alferdiansyah@it.student.pens.ac.id>
2020-07-08 03:43:13 +0700

    bikin konten sm link di hlmn akademik

--- app/Views/pages/akademik.php
+++ app/Views/pages/akademik.php
@@ -66,7 +66,7 @@
                 <h2 class="heading text-white mb-3">Jurusan</h2>
                 <div class="jurusan-carousel owl-carousel owl-theme ">
                     <a href="<?= current_url(); ?>/jurusan-elka" class="btn jurusan-carousel-item rounded-pill">
-                        Teknik Elektronika
+                        <span>Teknik Elektronika</span>
                     </a>
                     <div class="jurusan-carousel-item rounded-pill">
                         <p>Teknik Telekomunikasi</p>
