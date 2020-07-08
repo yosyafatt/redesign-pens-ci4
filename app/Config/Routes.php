bb16da7ec41df9b9ced4e73fd536a5a25003dd36
Yosyafat Aulia Ferdiansyah <alferdiansyah@it.student.pens.ac.id>
2020-07-08 08:22:35 +0700

    bikin konten sm link di hlmn akademik

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -32,7 +32,7 @@ $routes->setAutoRoute(true);
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Pages::index');
-$routes->get('/(:segment)', 'Pages::$1');
+$routes->get('/pages/(:segment)', 'Pages::$1');
 
 /**
  * --------------------------------------------------------------------
