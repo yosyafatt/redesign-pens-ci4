4098d064b2e5a7e6f37db72606e9e389170dc1f2
Yosyafat Aulia Ferdiansyah <alferdiansyah@it.student.pens.ac.id>
2020-07-12 12:59:35 +0700

    style dan menu tentang pens

--- app/Views/pages/tentang.php
+++ app/Views/pages/tentang.php
@@ -58,6 +58,9 @@
                     </ul>
                 </div>
             </div>
+            <div class="text-center">
+                <a href="<?= current_url(); ?>/visi-misi" class="btn shadow-sm btn-custom">Selengkapnya <span class="ml-4"><i class="fas fa-arrow-right"></i></span></a>
+            </div>
         </div>
     </section>
     <section class="text-center my-5">
