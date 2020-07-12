08e10e24e5c923936aa1a9399eceb904f87845ab
Yosyafat Aulia Ferdiansyah <alferdiansyah@it.student.pens.ac.id>
2020-07-12 13:53:20 +0700

    style dan menu tentang pens

--- app/Views/pages/beranda.php
+++ app/Views/pages/beranda.php
@@ -130,21 +130,18 @@
                     <div class="card-body">
                         <h4 class="heading card-title">Diploma III</h4>
                         <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                     </div>
                 </div>
                 <div class="card mb-4">
                     <div class="card-body">
                         <h4 class="heading card-title">Sarjana Terapan</h4>
                         <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                     </div>
                 </div>
                 <div class="card">
                     <div class="card-body">
                         <h4 class="heading card-title">Pascasarjana</h4>
                         <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                     </div>
                 </div>
             </div>
@@ -176,7 +173,7 @@
 <section class="beranda agenda" id="agenda">
     <div class="container">
         <h1 class="heading">Agenda</h1>
-        <div class="row">
+        <div class="row text-center">
             <p class="lead my-4">Belum ada agenda</p>
         </div>
     </div>
