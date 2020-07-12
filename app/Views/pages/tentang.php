40039e59d02e874d5451f63f437ed9e9c3ce5042
Yosyafat Aulia Ferdiansyah <alferdiansyah@it.student.pens.ac.id>
2020-07-12 12:29:01 +0700

    style dan menu tentang pens

--- app/Views/pages/tentang.php
+++ app/Views/pages/tentang.php
@@ -35,7 +35,7 @@
                 <div class="col">
                     <h1 class="heading mb-3">Selayang Pandang</h1>
                     <p>Awal sejarah PENS dimulai pada tahun 1985. Saat itu, tim studi awal Japan International Cooperation Agency (JICA) untuk bantuan dan kerjasama teknik yang dikepalai oleh Prof. Y. Naito dari Tokyo Institute of Technology, datang ke politeknik ini. Setelah melakukan pengamatan dan studi kelayakan di tahun 1986, JICA menyetujui untuk memulai kerjasama teknik di tahun 1987 dengan mengirim 5 orang pengajar Indonesia ke perguruan tinggi teknologi di Jepang.</p>
-                    <div>
+                    <div class="float-right">
                         <a href="<?= current_url(); ?>/sejarah-pens" class="btn shadow-sm btn-custom">Selengkapnya <span class="ml-4"><i class="fas fa-arrow-right"></i></span></a>
                     </div>
                 </div>
