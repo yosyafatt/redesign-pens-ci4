113b4afad2ae856e05f7165ec4fc49aa41287ee4
Yosyafat Aulia Ferdiansyah <alferdiansyah@it.student.pens.ac.id>
2020-07-12 12:23:39 +0700

    style dan menu tentang pens

--- app/Views/pages/tentang.php
+++ app/Views/pages/tentang.php
@@ -24,33 +24,16 @@
 <main class="main-content">
     <section>
         <div class="container">
+            <h2>Selayang Pandang</h2>
             <div class="row">
-                <div class="col-md-8">
-                    <!-- <h3 class="berita-title heading text-center">Sejarah PENS</h3> -->
-
-                    <div class="berita-content">
-                        <figure class="figure">
-                            <img src="<?= base_url(); ?>/public/img/tentang-pens.jpg" class="figure-img img-fluid" alt="...">
-                            <figcaption class="figure-caption text-right">Gedung D4 PENS</figcaption>
-                        </figure>
-                        <p>Awal sejarah PENS dimulai pada tahun 1985. Saat itu, tim studi awal Japan International Cooperation Agency (JICA) untuk bantuan dan kerjasama teknik yang dikepalai oleh Prof. Y. Naito dari Tokyo Institute of Technology, datang ke politeknik ini. Setelah melakukan pengamatan dan studi kelayakan di tahun 1986, JICA menyetujui untuk memulai kerjasama teknik di tahun 1987 dengan mengirim 5 orang pengajar Indonesia ke perguruan tinggi teknologi di Jepang...
-                        </p>
-                    </div>
+                <div class="col">
+                    <figure class="figure">
+                        <img src="<?= base_url(); ?>/public/img/tentang-pens.jpg" class="figure-img img-fluid" alt="...">
+                        <figcaption class="figure-caption text-right">Gedung D4 PENS</figcaption>
+                    </figure>
                 </div>
-                <div class="col-md-4">
-                    <aside class="sidebar">
-                        <h5 class="heading">Tentang PENS</h5>
-                        <ul>
-                            <li><a href="">Sejarah PENS</a></li>
-                            <li><a href="">Hymne PENS</a></li>
-                            <li><a href="">Visi dan Misi</a></li>
-                            <li><a href="">Struktur Organisasi</a></li>
-                            <li><a href="">Peraturan Akademik</a></li>
-                            <li><a href="">Kalender Akademik</a></li>
-                            <li><a href="">Akses Menuju Kampus</a></li>
-                            <li><a href="">Pedoman Sistem Identitas</a></li>
-                        </ul>
-                    </aside>
+                <div class="col">
+                    Awal sejarah PENS dimulai pada tahun 1985. Saat itu, tim studi awal Japan International Cooperation Agency (JICA) untuk bantuan dan kerjasama teknik yang dikepalai oleh Prof. Y. Naito dari Tokyo Institute of Technology, datang ke politeknik ini. Setelah melakukan pengamatan dan studi kelayakan di tahun 1986, JICA menyetujui untuk memulai kerjasama teknik di tahun 1987 dengan mengirim 5 orang pengajar Indonesia ke perguruan tinggi teknologi di Jepang.
                 </div>
             </div>
         </div>
